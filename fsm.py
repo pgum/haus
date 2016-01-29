9788dbd5f3690cd2fc4bcc4d522827ab6e2f45fc
killme@borg <killme@borg>
2016-01-29 01:45:12 +0100

    parsing added

--- fsm.py
+++ fsm.py
@@ -1,66 +1,107 @@
-
 class Message():
     def __init__(self, to="", content=""):
-        self.to=to
+        self.recipient=to
         self.content=content
 
 class ModuleBase():
     def __init__(self, parent=0):
-        self.moduleName=type(self).__name__
+        self.name=type(self).__name__
         self.moduleNodes={}
         self.parent=parent
 
     def isMessageToMe(self, message):
-        return message.to == self.moduleName
+        return self.name[:len(message.recipient)]==message.recipient
 
-    def isModuleLoaded(self, moduleName):
-        return moduleName in self.moduleNodes.keys()
+    def haveSubmodule(self, name):
+        return name in self.moduleNodes.keys()
 
-    def doesHaveParent(self):
+    def haveParent(self):
         return self.parent != 0
+
     def processMessage(self, message):
         if self.isMessageToMe(message):
             self.handleMessage(message)
-        elif self.isModuleLoaded(message.to):
-            self.moduleNodes[message.to].processMessage(message)
-        elif self.doesHaveParent():
+        elif self.haveSubmodule(message.recipient):
+            self.moduleNodes[message.recipient].processMessage(message)
+        elif self.haveParent():
             self.emitMessage(message)
         pass
 
     def addModule(self, module):
-        if not self.isModuleLoaded(module.moduleName):
+        if not self.haveSubmodule(module.name):
             module.parent=self
-            self.moduleNodes[module.moduleName]= module
+            self.moduleNodes[module.name]= module
 
-    def deleteModule(self, moduleName):
-        if self.isModuleLoaded(moduleName):
-           del self.moduleNodes[moduleName]
+    def addModuleByType(self, moduleName):
+        try:
+            self.addModule(globals()[moduleName]())
+        except KeyError as e:
+            pass
+
+    def deleteModule(self, name):
+        if self.haveSubmodule(name):
+           del self.moduleNodes[name]
+
+    def deleteAllModules(self):
+        for module in self.moduleNodes.keys():
+            self.deleteModule(module)
 
     def handleMessage(self,message):
         pass
-    def emitMessage(self, message):
-        if self.doesHaveParent():
-            self.parent.processMessage(message)
+    def parseMessage(self,message):
+        pass
 
+    def emitMessage(self, message):
+        if self.haveParent():
+            self.parent.processMessage(message)
+        else:
+            self.processMessage(message)
+
+    def forwardMessage(self, message, recipient):
+        message.recipient=recipient
+        self.emitMessage(message)
+
+    def writeToDebug(self, message):
+        self.forwardMessage(message, "WriterModule")
 
 class WriterModule(ModuleBase):
     def __init__(self):
-        super(type(self),self).__init__()
+        super(WriterModule,self).__init__()
+
+    def parseMessage(self, message):
+        return (self.name, message.recipient, message.content)
+
     def handleMessage(self, message):
-        print("ModuleName: %s Message Header | To: %s Content: %s " % (self.moduleName, message.to, message.content))
+        msgName, msgTo, msgContent = self.parseMessage(message)
+        print("ModuleName: %s Message Header | To: %s Content: %s " % (msgName, msgTo, msgContent))
+class WriterModule2(WriterModule):
+    def __init__(self):
+        super(type(self),self).__init__()
+class WriterModule3(WriterModule):
+    def __init__(self):
+        super(type(self),self).__init__()
+
+import simplejson as json
 
 class MasterModule(ModuleBase):
     def __init__(self):
-        super(type(self),self).__init__()
-    def handleMessage(self,message):
-        print("Master!!!!")
+        super(MasterModule,self).__init__()
 
-mm=MasterModule()
-wm=WriterModule()
-mm.addModule(wm)
-msg=Message("WriterModule","DEEDBEEF")
-msg2=Message("MasterModule","NVM")
-mm.moduleNodes["WriterModule"].processMessage(msg2)
-mm.processMessage(msg)
-mm.processMessage(msg2)
+
+    def parseMessage(self, message):
+        try:
+            msg=json.loads(message.content)
+            if msg["action"] == "add":
+                self.addModuleByType(msg["mName"])
+            elif msg["action"] == "delete":
+                self.deleteModule(msg["mName"])
+            else:
+                print("wtf?!")
+        except json.JSONDecodeError as e:
+            print("bez akcji: %s" %(message.content))
+
+
+    def handleMessage(self,message):
+        self.writeToDebug(message)
+        self.parseMessage(message)
 
