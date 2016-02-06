af9f1abfd250f59ac6e059aa5d7d88c5bf8d3264
pgum <killmepl@gmail.com>
2016-02-06 22:28:15 +0000

    better look

--- hauser/hauser.py
+++ hauser/hauser.py
@@ -1,27 +1,18 @@
 from bottle import route, run, debug, request, template
 import sh
+
 def sendToArduino(command):                                                                                                
     sh.echo(command, _out="/dev/ttyACM0");                                                                                 
-                                                                                                                           
+
 @route('/')                                                                                                                
 def main_view():                                                                                                           
     return template('links_for_actions.tpl', msg= "")                                                                      
-@route('/light/<channel:int>', method='GET')                                                                               
-def light_sender(channel):                                                                                                 
+@route('/<device>/<channel:int>', method='GET')                                                                               
+def device_sender(device, channel):                                                                                                 
     action= request.GET.get('action','').strip()                                                                           
-    command="l%s%s;" % (action, channel)                                                                                   
-    message = "This would be echoed to Arduino Serial Port: %s" % (command)                                                
+    command="%s%s%s;" % (device[0], action, channel)                                                                                   
     sendToArduino(command)                                                                                                 
-    return template('links_for_actions.tpl', msg = message)                                                                
+    return template('links_for_actions.tpl', msg = command)                                                                
                                                                                                                            
-@route('/relay/<channel:int>', method='GET')                                                                               
-def relay_sender(channel):                                                                                                 
-    action= request.GET.get('action','').strip()                                                                           
-    command="r%s%s;" % (action, channel)                                                                                   
-    message = "This would be echoed to Arduino Serial Port: %s" % (command)                                                
-    sendToArduino(command)                                                                                                 
-    return template('links_for_actions.tpl', msg = message)                             
-
 debug(True)
-
 run(host='0.0.0.0', port=80, reloader=True)
--- hauser/links_for_actions.tpl
+++ hauser/links_for_actions.tpl
@@ -1,13 +1,42 @@
-Remote lights:
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.on {
+    background-color: #4CAF50;
+}
+.off {
+    background-color: #f44336; 
+}
+.button {
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+</style>
+</head>
+<body>
+
+
 <ul>
-<li>Light A ("10000") - <a href="http://ditto/light/0?action=n">On</a> <a href="http://ditto/light/0?action=f">Off</a></li>
-<li>Light B ("01000") - <a href="http://ditto/light/1?action=n">On</a> <a href="http://ditto/light/1?action=f">Off</a></li>
-<li>Light D ("00010") - <a href="http://ditto/light/3?action=n">On</a> <a href="http://ditto/light/3?action=f">Off</a></li>
+<li><a href="http://ditto/light/0?action=n" class="button on">Light A - ON</a><a href="http://ditto/light/0?action=f" class="button off">Light A - OFF</a>
+<li><a href="http://ditto/light/1?action=n" class="button on">Light B - ON</a><a href="http://ditto/light/1?action=f" class="button off">Light B - OFF</a>
+<li><a href="http://ditto/light/3?action=n" class="button on">Light D - ON</a><a href="http://ditto/light/3?action=f" class="button off">Light D - OFF</a>
 </ul>
-Relay:
 <ul>
-<li>Input 1 ("IN1") - <a href="http://ditto/relay/0?action=n">On</a> <a href="http://ditto/relay/0?action=f">Off</a></li>
-<li>Input 2 ("IN2") - <a href="http://ditto/relay/1?action=n">On</a> <a href="http://ditto/relay/1?action=f">Off</a></li>
+<li><a href="http://ditto/relay/0?action=n" class="button on">Relay 1 - ON</a><a href="http://ditto/relay/0?action=f" class="button off">Relay 1 - OFF</a>
+<li><a href="http://ditto/relay/1?action=n" class="button on">Relay 2 - ON</a><a href="http://ditto/relay/1?action=f" class="button off">Relay 2 - OFF</a>
 </ul>
-Last command:</br>
-{{msg}}
+</br>
+Last command: {{msg}}
+
+</body>
+</html>
+
+
