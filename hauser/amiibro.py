5c58ae5bbec6777ed8035271779cc3d8450c1f97
killme@borg <killme@borg>
2016-02-23 22:03:45 +0100

    refactoring

--- hauser/amiibro.py
+++ hauser/amiibro.py
@@ -1,77 +1,73 @@
 import simplejson as json
 import urllib, urllib2, base64
 class KodiTalker:
+    def __init__(self):
+        self.url = "http://nest:8090/jsonrpc"
+        self.playerId=1
+        self.header=self.prepare_credentials("kodi:kodi")
+
+    def prepare_credentials(self,credentials):
+        encoded_credentials = base64.b64encode(self.credentials)
+        authorization = b'Basic ' + encoded_credentials
+        return { 'Content-Type': 'application/json', 'Authorization': authorization }
+
     def prepare_json(self, method, params):
-        return {'jsonrpc': '2.0',
-                'method' : method,
-                'params' : params,
-                'id'     : 1}
-     
+        message= {'jsonrpc': '2.0',
+                  'method' : method,
+                  'params' : params,
+                  'id'     : self.playerId}
+        return self.encode_command(message)
+
+
+    def encode_command(self, preparedJson):
+        json_data = json.dumps(preparedJson)
+        post_data = json_data.encode('utf-8')
+        return post_data
+
+    def send_command(self,command, times=1):
+        results=[]
+        req = urllib2.Request(self.url, command, self.headers)
+        for i in range(times): results.append(urllib2.urlopen(req))
+        print(results)
+        return results
+
+    def PlayPause(self):
+        jsonToSend= self.prepare_json('Player.PlayPause', { 'playerid': self.playerId})
+        return append(send_command(jsonToSend), times)
+
+    def SongSkip(self, prev_or_next, times = 1):
+        jsonToSend= self.prepare_json('Player.GoTo', { 'playerid': self.playerId, 'to': prev_or_next})
+        return append(send_command(jsonToSend), times)
+
+    def SongNext(self):
+        return self.SongSkip("next")
+
+    def SongPrev(self):
+        return self.SongSkip("prev")
+
+    def Volume(self,amount, increment_or_decrement):
+        jsonToSend= self.prepare_json("Application.SetVolume", { "volume": increment_or_decrement })
+        return send_command(jsonToSend,amount)
+
+    def VolumeUp(amount):
+        return self.Volume(amount,"increment")
+
+    def VolumeDown(amount):
+        return self.Volume(amount,"decrement")
+
+    def PlayYoutubeSong(videoId):
+        jsonToSend= self.prepare_json("Player.Open", { "item":{ "file": "plugin://plugin.video.youtube/?action=play_video&videoid=" + str(videoId)}})
+        return send_command(jsonToSend)
 
 class Amiibro:
+    def __init__(self):
+        self.kodiTalker = KodiTalker()
+        self.amiibos={
+                "04625FAA554980": self.kodiTalker.PlayPause(),
+                "0457ABE29A3D80": self.kodiTalker.VolumeUp(10),
+                "040C9A0AFE3D81": self.kodiTalker.VolumeDown(10)}
 
     def broforce(self, hex=None):
-        credentials = b'kodi:kodi'
-        encoded_credentials = base64.b64encode(credentials)
-        authorization = b'Basic ' + encoded_credentials
-        headers = { 'Content-Type': 'application/json', 'Authorization': authorization }
-
-        next_values = {'jsonrpc': '2.0',
-                  'method' : 'Player.GoTo',
-                  'params' : { 'playerid': 1, 
-                               'to': 'next'},
-                  'id'     : 1}
-        prev_values = {'jsonrpc': '2.0',
-                  'method' : 'Player.GoTo',
-                  'params' : { 'playerid': 1, 
-                               'to': 'previous'},
-                  'id'     : 1}
-        vol_up = { "jsonrpc": "2.0", "method": "Application.SetVolume", "params": { "volume": "increment" }, "id": 1 }
-        vol_down = { "jsonrpc": "2.0", "method": "Application.SetVolume", "params": { "volume": "decrement" }, "id": 1 }
-        values = {'jsonrpc': '2.0',
-                  'method' : 'Player.PlayPause',
-                  'params' : { 'playerid': 1},
-                  'id'     : 1}
-        send_song_values = { "jsonrpc":"2.0",
-                             "id":"1",
-                             "method":"Player.Open",
-                             "params":{
-                                  "item":{
-                                      "file":"plugin://plugin.video.youtube/?action=play_video&videoid=iNEIpa5fpYg"}}}
-
-        json_data = json.dumps(values)
-        post_data = json_data.encode('utf-8')
-
-        json_data1= json.dumps(send_song_values)
-        post_data1 = json_data1.encode('utf-8')
-
-        json_data3= json.dumps(vol_up)
-        post_datan = json_data3.encode('utf-8')
-
-        json_data2= json.dumps(vol_down)
-        post_datap = json_data2.encode('utf-8')
-
-
-        url = "http://nest:8090/jsonrpc"
-        reti = hex
-        req = 0
-        step=10
-        if(hex == "04625FAA554980"):
-            req = urllib2.Request(url, post_data, headers)
-            result = urllib2.urlopen(req)
-            print("Play/Pause Mewtwo!")
-            reti = "Mewtwo"
-        if(hex == "0457ABE29A3D80"):
-            req = urllib2.Request(url, post_datan, headers)
-            for i in range(0, step): result = urllib2.urlopen(req)
-            print("Up Pikachu")
-            reti = "Pikachu"
-        if(hex == "040C9A0AFE3D81"):
-            print("Down Kirby")
-            req = urllib2.Request(url, post_datap, headers)
-            for i in range(0, step): result = urllib2.urlopen(req)
-            reti = "Kirby"
-        if(req != 0):
-            print(result.read())
-        return dict(msg=reti)
+        if hex in self.amiibos:
+            return dict(msg= self.amiibos[hex]())
 
