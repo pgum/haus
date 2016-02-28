26e81af5548b6d925adf53b713097efddf54dcae
killme@borg <killme@borg>
2016-02-28 02:48:40 +0100

    remove newline

--- hauser/koditalker.py
+++ hauser/koditalker.py
@@ -21,7 +21,6 @@ class KodiTalker:
                   'id'     : self.playerId}
         return self.encode_command(message)
 
-
     def encode_command(self, preparedJson):
         json_data = json.dumps(preparedJson)
         post_data = json_data.encode('utf-8')
@@ -57,6 +56,54 @@ class KodiTalker:
 
     def VolumeDown(self, amount):
         return self.Volume(amount,"decrement")
+
+    def PlayYoutubeSong(self, videoId):
+        jsonToSend= self.prepare_json("Player.Open",
+                { "item": { "file": "plugin://plugin.video.youtube/?action=play_video&videoid=" + str(videoId)}})
+        return self.send_command(jsonToSend)
+
+import requests
+class KodiTalker2:
+    def __init__(self):
+        self.url = "http://nest:8090/jsonrpc"
+        self.playerId=1
+        self.auth=(b"kodi",b"kodi")
+
+    def prepare_json(self, method, params):
+        return {'jsonrpc': '2.0',
+                'method' : method,
+                'params' : params,
+                'id'     : self.playerId}
+
+    def send_command(self,command, times=1):
+        req = requests.get(self.url, params= {'request': command}, auth=self.auth)
+        print(req.text)
+        return req.text
+
+    def PlayPause(self):
+        jsonToSend= self.prepare_json('Player.PlayPause', { 'playerid': self.playerId})
+        self.send_command(jsonToSend)
+
+    def SongSkip(self, prev_or_next, times = 1):
+        jsonToSend= self.prepare_json('Player.GoTo', { 'playerid': self.playerId, 'to': prev_or_next})
+        self.send_command(jsonToSend, times)
+
+    def SongNext(self):
+        self.SongSkip("next")
+
+    def SongPrev(self):
+        self.SongSkip("prev")
+
+    def Volume(self,amount, increment_or_decrement):
+        jsonToSend= self.prepare_json("Application.SetVolume", { "volume": increment_or_decrement })
+        for i in range(times):
+            self.send_command(jsonToSend, amount)
+
+    def VolumeUp(self, amount):
+        self.Volume(amount,"increment")
+
+    def VolumeDown(self, amount):
+        self.Volume(amount,"decrement")
 
     def PlayYoutubeSong(self, videoId):
         jsonToSend= self.prepare_json("Player.Open",
