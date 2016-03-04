ca0c22ad4b869f6d46050e436c705232be64fdba
killme@borg <killme@borg>
2016-03-04 15:25:20 +0100

    ardubro over 2.4GHz

--- ardubro/ardubro.ino
+++ ardubro/ardubro.ino
@@ -4,9 +4,12 @@
 
 using MyRelay= RelayController<2>;
 MyRelay rc;
-RF24 radio(3,2)
 int lightSwitchPin = 11;
 bool lightSwitchState=false;
+
+RF24 radio(3,2);
+byte arduino_address[] = {"ardu1"};
+byte rpi_address[] = {"RpiC"};
 
 void setup(){
   int relayChannelsPins[] = {8,7}
@@ -14,9 +17,29 @@ void setup(){
   Serial.begin(9600);
   pinMode(lightSwitchPin, INPUT_PULLUP);
   lightSwitchState= digitalRead(lightSwitchPin);
+
+  radio.begin();
+
+  radio.setPALevel(RF24_PA_LOW); //potem do wywalenia jak beda daleko od siebie
+
+  radio.openWritingPipe(arduino_address);
+  radio.openReadingPipe(rpi_address);
+
+
+  radio.startListening();
 }
 
 void loop() {
+  char receivedCommandBuffor[5];
+  if(radio.available()){
+    while(radio.available()){
+      radio.read( receivedCommandBuffor, sizeof(receivedCommandBuffor));
+    }
+  if(receivedCommandBuffor[0] == 'r'){
+    int channel= (int)receivedCommandBuffor[1] - 48;
+    bool toState= receivedCommandBuffor[2] == 'n' ? true : false;
+    rc.setRelayToBe(channel, toState);
+  }
   bool lastState= lightSwitchState;
   lightSwitchState= digitalRead(lightSwitchPin);
   if(lastState != lightSwitchState){
