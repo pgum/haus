b65086767200d1dc7b672f08b27030ca7dde4602
killme@borg <killme@borg>
2016-03-05 17:13:21 +0100

    refactoring

--- ardubro/ardubro.ino
+++ ardubro/ardubro.ino
@@ -8,18 +8,18 @@ String convertedStatusToString(bool statusArray[], short num){
     if(statusArray[i]) reti += "n";
     else reti+= "f";
   return reti;
-  }
+}
 
 RelayController<2> relay;
 int lightSwitchPin = 11;
 bool lightSwitchState=false;
 
 RF24 radio(3,2);
-byte arduino_address[] = {"ardu1"};
-byte rpi_address[] = {"RpiC"};
-short relayChannelsPins[] = {8,7};
+byte arduino_address[]= {"ardu1"};
+byte rpi_address[]= {"RpiC"};
+short relayChannelsPins[]= {8,7};
+
 void setup(){
-  
   relay.initRelayController(relayChannelsPins);
   Serial.begin(9600);
   pinMode(lightSwitchPin, INPUT_PULLUP);
@@ -36,13 +36,13 @@ void setup(){
 void loop() {
   char receivedCommandBuffor[5];
   if(radio.available()){
-    while(radio.available())
-      radio.read( receivedCommandBuffor, sizeof(receivedCommandBuffor));
-    
-  if(receivedCommandBuffor[0] == 'r'){
-    int channel= (int)receivedCommandBuffor[1] - 48;
-    bool toState= receivedCommandBuffor[2] == 'n' ? true : false;
-    relay.setRelayToBe(channel, toState);
+    while(radio.available()
+      radio.read(receivedCommandBuffor, sizeof(receivedCommandBuffor));
+    if(receivedCommandBuffor[0] == 'r'){
+      int channel= (int)receivedCommandBuffor[1] - 48;
+      bool toState= receivedCommandBuffor[2] == 'n' ? true : false;
+      relay.setRelayToBe(channel, toState);
+    }
   }
   bool lastState= lightSwitchState;
   lightSwitchState= digitalRead(lightSwitchPin);
@@ -50,4 +50,4 @@ void loop() {
     relay.setRelayToBe(0,lightSwitchState);
   relay.update();
 }
-}
+
