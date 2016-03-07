3d55de91f3481647aa736c798b4f753bcaec77dd
killme@borg <killme@borg>
2016-03-07 21:07:50 +0100

    refactoring + new amiboo

--- hauser/hauser.py
+++ hauser/hauser.py
@@ -1,6 +1,7 @@
 from ardubro import Ardubro
 from amiibro import Amiibro
 from sockbro import Sockbro
+from koditalker import KodiTalker
 
 class Hauser:
     def __init__(self):
@@ -10,9 +11,13 @@ class Hauser:
                 'amiibo' : Amiibro(),
                 'sockets': Sockbro()
                 }
-
-    def status(self):
-        return dict(msg = "", responses= self.responses)
+        self.devices['amiibo'].amiibos={
+                "04625FAA554980": {'name': "Mewtwo" , 'method': KodiTalker().PlayPause , 'params': None},
+                "0457ABE29A3D80": {'name': "Pikachu", 'method': KodiTalker().VolumeUp  , 'params': 20},
+                "040C9A0AFE3D81": {'name': "Kirby"  , 'method': KodiTalker().VolumeDown, 'params': 20},
+                "043BAE92BF4881": {'name': "Mario"  , 'method': self.devices['sockets'].switchOn, 'params': 4},
+                "041C9982034980": {'name': "DuckHunt", 'method': self.devices['sockets'].switchOn, 'params': 4},
+                }
 
     def requestActionOnDevice(self, device, action, channel):
         if device in self.devices:
--- hauser/hauser_web.py
+++ hauser/hauser_web.py
@@ -1,20 +1,14 @@
 from bottle import route, run, debug, template, view, static_file
 from hauser import Hauser
-from koditalker import KodiTalker
 
 haus = Hauser()
-haus.devices['amiibo'].amiibos={
-                "04625FAA554980": {'name': "Mewtwo" , 'method': KodiTalker().PlayPause , 'params': None},
-                "0457ABE29A3D80": {'name': "Pikachu", 'method': KodiTalker().VolumeUp  , 'params': 20},
-                "040C9A0AFE3D81": {'name': "Kirby"  , 'method': KodiTalker().VolumeDown, 'params': 20}}
 
 def makeDict(what, channel, action):
-    #return dict(msg = "%s action: Turn %s %s; Response: %s" % (what, channel, action, haus.responses[-1]), responses = haus.responses)
     return dict(msg = "%s action: Turn %s %s" % (what, channel, action), responses = haus.responses)
 
 @route('/')
 @view('main.tpl')
-def status():
+def nop():
     return dict(msg = "~-~-~-~-~", responses= haus.responses)
 
 @route('/<device>/<action>/<channel:int>')
@@ -23,6 +17,7 @@ def deviceActionRequest(device=None, action=None, channel=None):
     haus.requestActionOnDevice(device, action, channel)
     return makeDict(device, channel, action)
 
+@route('/amigo/<tag>')
 @route('/amiicode/<tag>')
 def amiiboNfcTagRequest(tag=None):
     msgs="Not Hex Amigo"
