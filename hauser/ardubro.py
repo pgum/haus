5a865f231088edd08b55b099fefcd91f1b6c3be3
killme@ditto <killme@ditto>
2016-03-24 22:55:13 +0000

    turned off for debuging purposes

--- hauser/ardubro.py
+++ hauser/ardubro.py
@@ -1,15 +1,12 @@
-from nfr24 import NRF24
+#from nfr24 import NRF24
 import time
-
-
-
 
 class Ardubro():
     def __init__(self, Aaddress="ardu1", Raddress="rasp1", relays= 2):
         self.arduinoAddress= Aaddress
         self.raspberryAddress= Raddress
         self.relays= [True for relay in range(relays)]
-        self.radio = NRF24()
+        #self.radio = NRF24()
 
     def switchOn(self, relay):
         self._switchRelay(relay, True)
@@ -21,7 +18,7 @@ class Ardubro():
         self._switchRelay(relay, not self.relays[relay])
 
     def _switchRelay(self, relay, state):
-        self.relays[relay]= state
+        self.relays[int(relay)]= state
         self._sendCommand("")
 
     def _sendCommand(self, command):
