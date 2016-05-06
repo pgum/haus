61d61ae9657f8b1206d2b49831900a8c33091195
killme@borg <killme@borg>
2016-05-06 22:59:39 +0200

    refactoring

--- ardubro/LogicalPin.h
+++ ardubro/LogicalPin.h
@@ -1,15 +1,55 @@
+#ifndef LogicalPin_ONCE
+#define LogicalPin_ONCE
+#include <Arduino.h>
+
+typedef short channel;
+typedef short nunberOfChannels;
+typedef bool state;
+
 class LogicalPin{
-  short pin;
-  bool pinStatus;
-  bool low, high;
-  
+  channel pin;
+  state s;
 public:
-  LogicalPin(): pinStatus(false), pin(-1){}
-  void setPhysicalPin(int physicalPin);
-  void initRelayPin();
-  bool turnOn();
-  bool turnOff();
-  bool toggle();
-  bool getStatus();
+  LogicalPin(): pin(-1), s(false){}
+  LogicalPin(short physicalPin): pin(physicalPin), s(false){}
+  void initPin(short physicalPin){
+    pin= physicalPin;
+    pinMode(pin, OUTPUT);
+  }
+  void turnOn(){
+    s=true;
+    digitalWrite(pin, LOW);
+  }
+  void turnOff(){
+    s=false;
+    digitalWrite(pin, HIGH);
+  }
+  void toggle(){
+    if(s) turnOff();
+    else turnOn();
+  }
+  const bool getState(){
+      return s;
+  }
 };
 
+template <nunberOfChannels N> class LogicalPins{
+    LogicalPin pinData[N];
+    state pinStatus[N], isInitialized;
+public:
+  LogicalPins() : pinData(), isInitialized(false){}
+  LogicalPin &operator[](channel index){
+    return pinData[index];
+  }
+  void initPins(channel tableOfPhysicalPinsForRelayInputs[]){
+    for(channel i= 0; i< N; ++i) 
+      pinData[i].initPin(tableOfPhysicalPinsForRelayInputs[i]);
+    isInitialized= true;
+  }
+  state* getStatus(){
+    for(channel i= 0; i< N; ++i)
+      pinStatus[i]= pinData[i].getState();
+    return pinStatus;
+  }
+};
+#endif
--- ardubro/RelayController.h
+++ ardubro/RelayController.h
@@ -1,56 +1,30 @@
 #ifndef RelayController_ONCE
 #define RelayController_ONCE
 #include "LogicalPin.h"
+typedef short channel;
+typedef short nunberOfChannels;
+typedef bool state;
 
-template<int N>
-class RelayController{
-  LogicalPin relayChannel[N];
-  bool statusToBeAfterUpdate[N];
+template<nunberOfChannels N> class RelayController{
+  LogicalPins<N> relays;
+  state relaysNextStates[N];
 public:
-  RelayController(){}
-
-  void turnOn(short int input);
-  void turnOff(short int input);
-  void initRelayController(short int tableOfPhysicalPinsForRelayInputs[]);
-  bool getRelayStatus();
-  void setRelayToBe(int i, bool state);
-  void update();
+  void init(channel tableOfPhysicalPinsForRelayInputs[]){
+    relays.initPins(tableOfPhysicalPinsForRelayInputs);
+  }
+  void makeOn(channel id){
+    relaysNextStates[id]= true;
+  }
+  void makeOff(channel id){
+    relaysNextStates[id]= false;
+  }
+  void updateRelays(){
+    for(channel i= 0; i< N; ++i)
+      if(relaysNextStates[i]) relays[i].turnOn();
+      else relays[i].turnOff();
+  }
+  const bool* getStatus(){
+    return relays.getStatus();
+  }
 };
 #endif
-
-template<int N>
-void RelayController<N>::turnOn(short int input){
-  relayChannel[input].turnOn();
-}
-template<int N>
-void RelayController<N>::turnOff(short int input){
-    relayChannel[input].turnOff();
-  }
-template<int N>
-void RelayController<N>::initRelayController(short int tableOfPhysicalPinsForRelayInputs[]){
-  for(int i= 0; i< N; ++i){
-    relayChannel[i].setPhysicalPin(tableOfPhysicalPinsForRelayInputs[i]);
-    relayChannel[i].initRelayPin();
-  }
-}
-template<int N>
-bool RelayController<N>::getRelayStatus(){
-  bool statuses[N];
-  for(int i= 0; i< N; ++i){
-    statuses[i]=relayChannel[i].getStatus();
-  }
-  return statuses;
-}
-template<int N>
-void RelayController<N>::setRelayToBe(int i, bool state){
-  if(i > N){ return; }
-  statusToBeAfterUpdate[i]=state;
-}
-template<int N>
-void RelayController<N>::update(){
-  for(int i= 0; i< N; ++i){
-    if(statusToBeAfterUpdate[i]){ turnOn(i); }
-    else{ turnOff(i); }
-  }
-}
-
