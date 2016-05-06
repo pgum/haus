b24e08644d6a33bd7eb9be150a8948ecb212a9a3
killme@borg <killme@borg>
2016-05-06 23:00:10 +0200

    radio not working

--- ardubro/ardubro.ino
+++ ardubro/ardubro.ino
@@ -1,6 +1,6 @@
 #include "RelayController.h"
 #include <SPI.h>
-#include <RF24.h>
+//#include <RF24.h>
 
 String convertedStatusToString(bool statusArray[], short num){
   String reti;
@@ -14,40 +14,46 @@ RelayController<2> relay;
 int lightSwitchPin = 11;
 bool lightSwitchState=false;
 
-RF24 radio(3,2);
+//RF24 radio(3,2);
 byte arduino_address[]= {"ardu1"};
 byte rpi_address[]= {"RpiC"};
 short relayChannelsPins[]= {8,7};
-
+/*
+void initRadio(){
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW); //potem do wywalenia jak beda daleko od siebie
+  radio.openWritingPipe(arduino_address);
+  radio.openReadingPipe(0, rpi_address);
+  radio.startListening();
+}*/
 void setup(){
   relay.initRelayController(relayChannelsPins);
   Serial.begin(9600);
   pinMode(lightSwitchPin, INPUT_PULLUP);
   lightSwitchState= digitalRead(lightSwitchPin);
 
-  radio.begin();
-  radio.setPALevel(RF24_PA_LOW); //potem do wywalenia jak beda daleko od siebie
-  radio.openWritingPipe(arduino_address);
-  radio.openReadingPipe(0, rpi_address);
-
-  radio.startListening();
+  //initRadio();
 }
-
-void loop() {
+/*void handleRadio(){
   char receivedCommandBuffor[5];
   if(radio.available()){
     while(radio.available())
       radio.read(receivedCommandBuffor, sizeof(receivedCommandBuffor));
     if(receivedCommandBuffor[0] == 'r'){
       int channel= (int)receivedCommandBuffor[1] - 48;
-      bool toState= receivedCommandBuffor[2] == 'n' ? true : false;
-      relay.setRelayToBe(channel, toState);
+      bool toState= receivedCommandBuffor[2] == 'n';
+      relay.setState(channel, toState);
     }
   }
+}*/
+void loop() {
+  //handleRadio();
   bool lastState= lightSwitchState;
   lightSwitchState= digitalRead(lightSwitchPin);
-  if(lastState != lightSwitchState)
-    relay.setRelayToBe(0,lightSwitchState);
+  if(lastState != lightSwitchState && lightSwitchState)
+    relay.makeOn(0);
+  else
+    relay.makeOff(0);
   relay.update();
 }
 
