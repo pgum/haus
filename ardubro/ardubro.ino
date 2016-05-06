ad9f5b88579c630bfa99e705db11dc3f070988f8
killme@borg <killme@borg>
2016-05-06 23:12:39 +0200

    refactoring

--- ardubro/ardubro.ino
+++ ardubro/ardubro.ino
@@ -1,39 +1,16 @@
 #include "RelayController.h"
-#include <SPI.h>
-//#include <RF24.h>
-
-String convertedStatusToString(bool statusArray[], short num){
-  String reti;
-  for(int i= 0; i< num; ++i)
-    if(statusArray[i]) reti += "n";
-    else reti+= "f";
-  return reti;
-}
-
-RelayController<2> relay;
-int lightSwitchPin = 11;
-bool lightSwitchState=false;
-
-//RF24 radio(3,2);
-byte arduino_address[]= {"ardu1"};
-byte rpi_address[]= {"RpiC"};
-short relayChannelsPins[]= {8,7};
-/*
-void initRadio(){
+/*#include <SPI.h>
+#include <RF24.h>
+RF24 radio(3,2);*/
+/*void initRadio(){
+  byte arduino_address[]= {"ardu1"};
+  byte rpi_address[]= {"RpiC"};
   radio.begin();
   radio.setPALevel(RF24_PA_LOW); //potem do wywalenia jak beda daleko od siebie
   radio.openWritingPipe(arduino_address);
   radio.openReadingPipe(0, rpi_address);
   radio.startListening();
 }*/
-void setup(){
-  relay.initRelayController(relayChannelsPins);
-  Serial.begin(9600);
-  pinMode(lightSwitchPin, INPUT_PULLUP);
-  lightSwitchState= digitalRead(lightSwitchPin);
-
-  //initRadio();
-}
 /*void handleRadio(){
   char receivedCommandBuffor[5];
   if(radio.available()){
@@ -41,12 +18,35 @@ void setup(){
       radio.read(receivedCommandBuffor, sizeof(receivedCommandBuffor));
     if(receivedCommandBuffor[0] == 'r'){
       int channel= (int)receivedCommandBuffor[1] - 48;
-      bool toState= receivedCommandBuffor[2] == 'n';
-      relay.setState(channel, toState);
+      bool isToBeOn= receivedCommandBuffor[2] == 'n';
+      if(isToBeOn) relay.makeOn(channel);
+      else relay.makeOff(channel);
     }
   }
 }*/
-void loop() {
+/*String convertedStatusToString(bool statusArray[], short num){
+  String reti;
+  for(short i= 0; i< num; ++i)
+    if(statusArray[i]) reti += "n";
+    else reti+= "f";
+  return reti;
+}*/
+
+const int lightSwitchPin = 11;
+const short relayChannelsPins[]= {8,7};
+
+RelayController<2> relay;
+bool lightSwitchState=false;
+
+void setup(){
+
+  relay.init(relayChannelsPins);
+  pinMode(lightSwitchPin, INPUT_PULLUP);
+  lightSwitchState= digitalRead(lightSwitchPin);
+  //initRadio();
+}
+
+void loop(){
   //handleRadio();
   bool lastState= lightSwitchState;
   lightSwitchState= digitalRead(lightSwitchPin);
@@ -56,4 +56,3 @@ void loop() {
     relay.makeOff(0);
   relay.update();
 }
-
